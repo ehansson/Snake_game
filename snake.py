cc66501b0d40750a41da2d54e09e94e67003ffb0
ehansson <91335074+ehansson@users.noreply.github.com>
2024-01-25 18:51:25 +0100

    Added some updates
    Restructured the code to easier add widgets and changes. Also added a start and stopbutton, as well as a username for the score board.

--- snake.py
+++ snake.py
@@ -21,7 +21,7 @@ USERNAME = "Dangernoodle"
 
 
 class Snake():
-    def __init__(self):
+    def __init__(self, canvas):
         self.bodylength = STARTLENGTH
         self.coords = []
         self.squares = []
@@ -39,114 +39,164 @@ class Snake():
 
         
 class Food():
-    def __init__(self):
+    def __init__(self, canvas):
         x =random.randint(0, SCREENWIDTH/GRID-1)*GRID
         y = random.randint(0, SCREENHIGHT/GRID-1)*GRID
+        self.canvas = canvas
         self.coords = [x,y]
         self.circle = canvas.create_oval(x,y,x+GRID,y+GRID, 
                                          fill=FOODCOLOR, tag="food")
     def eaten(self):
-        canvas.delete(self.circle)
+        self.canvas.delete(self.circle)
         x =random.randint(0, SCREENWIDTH/GRID-1)*GRID
         y = random.randint(0, SCREENHIGHT/GRID-1)*GRID
         self.coords = [x,y]
-        self.circle = canvas.create_oval(x,y,x+GRID,y+GRID, 
-                                         fill=FOODCOLOR, tag="food")
-
-
-def keybinds(keypress):
-    global direction
-    if keypress.keycode == 37 and direction != "right":
-        direction = "left"
-    if keypress.keycode == 38 and direction != "down":
-        direction = "up"
-    if keypress.keycode == 39 and direction != "left":
-        direction = "right"
-    if keypress.keycode == 40 and direction != "up":
-        direction = "down"   
-    if keypress.keycode == 32:
-        canvas.delete(instructions)
-        while True:
-            try: 
-                next_move(snake, food)
-            except:  
-                break
-         
-def next_move(snake, food):
-    x,y = snake.coords[0]
-    
-    newx = x + directions[direction][0]
-    newy = y + directions[direction][1]
-    
-    snake.coords.insert(0, [newx, newy])
-    x, y = snake.coords[0]
-    square = canvas.create_rectangle(x, y, x+GRID, y+GRID, 
-                                     fill=SNAKECOLOR, tag="snake")
-    snake.squares.insert(0, square)
-    
-    
-    # If snake head is not at food, snake does not grow
-    if food.coords != snake.coords[0]:    
-        snake.coords.pop(-1)
-        canvas.delete(snake.squares[-1])
-        snake.squares.pop(-1)
+        self.circle = self.canvas.create_oval(x,y,x+GRID,y+GRID, 
+                                         fill=FOODCOLOR, tag="food") 
         
-    else:
-        food.eaten()
-        snake.score += 1
-    
-    # If snake leaves screen you die
-    if x < 0 or x > SCREENWIDTH-GRID or y < 0 or y > SCREENHIGHT-GRID:
-        print('You crashed into the wall!')
-        print("GAME OVER")
-        set_score(USERNAME, snake.score)
-        screen.destroy()
+class Game(tk.Tk):
+    def __init__(self, master = None):
+        tk.Tk.__init__(self, master)
+        self.direction = 'right' #Starting direction
+        self.username = tk.StringVar()
+        self.username.set(USERNAME)
+        self.add_widgets()
         
-    # If head touches body you die
-    if snake.coords[0] in snake.coords[1:]:
-        print('You bit yourself!')
-        print("GAME OVER")
-        set_score(USERNAME, snake.score)
-        screen.destroy()
+        self.grid()
+        
+
+        
+    def add_widgets(self):
+        self.usernameEntry = tk.Entry(self, textvariable=self.username)
+        self.usernameEntry.pack()
+
+        
+        self.startButton = tk.Button(
+            self, text="Start", command=lambda: [
+                self.destroy_startscreen(), self.create_game_screen()])
+        self.startButton.pack()
+        
+        self.quitButton = tk.Button(
+            self, text="Quit", command=self.destroy)
+        self.quitButton.pack()
+        
+        self.highscoreLabel = tk.Label(self, text="")
+        self.highscoreLabel.pack()
+        
+        self.highscoreButton = tk.Button(
+            self, text="Highscore", command=self.show_highscore)
+        self.highscoreButton.pack()
+        
+    def destroy_startscreen(self):
+        self.usernameEntry.pack_forget()
+        self.startButton.destroy()
+        self.quitButton.destroy()
+        self.highscoreButton.destroy()
+        self.highscoreLabel.destroy()
+
+    def create_game_screen(self):
+        # Create screen
+        self.canvas = tk.Canvas(self, bg=BACKGROUND, width=SCREENWIDTH, height=SCREENHIGHT)
+        self.canvas.pack()
+        
+        self.instructions = self.canvas.create_text(
+            SCREENWIDTH/2, SCREENHIGHT/2+GRID, text = "Press <space> to start", fill = "#FFFFFF")
+
+        self.snake = Snake(self.canvas)
+        self.food = Food(self.canvas)
+        self.bind_all("<Key>", self.keybinds) 
+        
+
+    def keybinds(self, keypress):
+        
+        if keypress.keycode == 37 and self.direction != "right":
+            self.direction = "left"
+        if keypress.keycode == 38 and self.direction != "down":
+            self.direction = "up"
+        if keypress.keycode == 39 and self.direction != "left":
+            self.direction = "right"
+        if keypress.keycode == 40 and self.direction != "up":
+            self.direction = "down"   
+        if keypress.keycode == 32:
+            self.canvas.delete(self.instructions)
+            while True:
+                try: 
+                    self.next_move(self.snake, self.food)
+                except:  
+                    break
+             
+    def next_move(self, snake, food):
+        x,y = snake.coords[0]
+        
+        # Dictionary of directions   
+        directions = {'up':[0,-GRID], 'down':[0,GRID], 'left':[-GRID,0], 'right':[GRID,0]}
+        
+        newx = x + directions[self.direction][0]
+        newy = y + directions[self.direction][1]
+        
+        snake.coords.insert(0, [newx, newy])
+        x, y = snake.coords[0]
+        square = self.canvas.create_rectangle(x, y, x+GRID, y+GRID, 
+                                         fill=SNAKECOLOR, tag="snake")
+        snake.squares.insert(0, square)
+        
+        
+        # If snake head is not at food, snake does not grow
+        if food.coords != snake.coords[0]:    
+            snake.coords.pop(-1)
+            self.canvas.delete(snake.squares[-1])
+            snake.squares.pop(-1)
+            
+        else:
+            food.eaten()
+            snake.score += 1
+        
+        # If snake leaves screen you die
+        if x < 0 or x > SCREENWIDTH-GRID or y < 0 or y > SCREENHIGHT-GRID:
+            print('You crashed into the wall!')
+            
+            print("GAME OVER")
+            
+            self.set_score(self.username.get(), snake.score)
+            self.canvas.destroy()
+            self.add_widgets()
+            
+        # If head touches body you die
+        if snake.coords[0] in snake.coords[1:]:
+            print('You bit yourself!')
+            print("GAME OVER")
+            self.set_score(self.username.get(), snake.score)
+            self.canvas.destroy()
+            self.add_widgets()
+        
+        time.sleep(SPEED)
+        self.canvas.update()
     
-    time.sleep(SPEED)
-    canvas.update()
-
-def set_score(username, score):
-    scoreboard = open("snake_score.txt", "a")
-    scoreboard.write(f'{USERNAME}: {score} \n')
-    scoreboard.close()
+    def set_score(self, username, score):
+        scoreboard = open("snake_score.txt", "a")
+        scoreboard.write(f'{username}: {score} \n')
+        scoreboard.close()
+        
+    def get_highscore(self):
+        scoreboard = open("snake_score.txt", "r")
+        lines = scoreboard.readlines()
+        highscore = 0
+        scoreholder = "Nobody"
+        for line in lines:
+            name, score = line.split(": ")
+            score = int(score)
+            if score > highscore:
+                highscore = score
+                scoreholder = name
+        return scoreholder, highscore
     
-def get_highscore():
-    scoreboard = open("snake_score.txt", "r")
-    lines = scoreboard.readlines()
-    highscore = 0
-    scoreholder = "Nobody"
-    for line in lines:
-        name, score = line.split(": ")
-        score = int(score)
-        if score > highscore:
-            highscore = score
-            scoreholder = name
-    return scoreholder, highscore
-    
+    def show_highscore(self):
+        scoreholder, highscore = self.get_highscore()
+        self.highscoreLabel.config(text = "The best player is " + scoreholder +
+                                   " with " + str(highscore) + "points!")
+        
+
+game = Game()
 
 
-# Dictionary of directions   
-directions = {'up':[0,-GRID], 'down':[0,GRID], 'left':[-GRID,0], 'right':[GRID,0]}
-direction = 'right' #Starting direction
-
-# Create screen
-screen = tk.Tk()
-canvas = tk.Canvas(screen, bg=BACKGROUND, width=SCREENWIDTH, height=SCREENHIGHT)
-canvas.pack()
-
-snake = Snake()
-food = Food()
-screen.bind("<Key>", keybinds)
-instructions = canvas.create_text(SCREENWIDTH/2, SCREENHIGHT/2+GRID, 
-                   text = "Press <space> to start", fill = "#FFFFFF")
-screen.mainloop()
-
-winner, highscore = get_highscore()
-print(f'The best player is {winner} with {highscore} points!')
+game.mainloop()
